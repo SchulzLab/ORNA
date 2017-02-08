763411de3836418023f4dbe5f325cd49361c1db9
ddurai <dilipdurai@yahoo.co.in>
2017-02-08 16:16:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
+#Initializing and updating the git submodules
 git submodule init;
 git submodule update;
 
+#Making a build directory. This is where the executable will be stored
 rm -rf build
 mkdir build
 
 cd build
 
+#Running the cmake command from the build directory
 cmake ..
 
+#Running the make command
 make -j 10
 
 cd ..
