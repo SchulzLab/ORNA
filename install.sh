8041869ea4fac7ef9af3fa093c35bacef4b279d2
ddurai <dilipdurai@yahoo.co.in>
2017-02-08 15:36:29 +0100

    Modified installation

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 git submodule init;
 git submodule update;
-bash gatb-core/gatb-core/scripts/NewProject/NewProject.sh -d $(pwd) -n ORNA;
-export cwd=$(pwd);
-mkdir ORNA/build;
-cd ORNA/build;
-cmake ..;
-make -j 10;
-echo $cwd;
-cp $cwd/src/main.cpp $cwd/ORNA/tools/ORNA/src/main.cpp;
-make -j 10;
+
+rm -rf build
+mkdir build
+
+cd build
+
+cmake ..
+
+make -j 10
+
+cd ..
