0b8ee0ab8588864807980c220783e2ac6a314055
ddurai <dilipdurai@yahoo.co.in>
2017-02-10 16:02:49 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -109,7 +109,7 @@ public:
 				for (itKmer.first(); !itKmer.isDone(); itKmer.next())
 				{
 					std::string s = model.toString (itKmer->value());
-					const char* sq = s.c_str();git commit -a 
+					const char* sq = s.c_str(); 
 					Node node = graph.buildNode(sq);
 					//Checking whether the node exists.
 					if(graph.contains(node))
